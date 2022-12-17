93274bef62c94390436336b8bedd6d116e3444b1
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-17 15:05:20 +0000

    Ajuste final

--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,7 +1,7 @@
 # Your code goes here:
 def render_person(name, date, color, age, gender):
-    return (name + " is a " + str(age) + " years old " + gender + " born in " + date + " with " + color + " eyes ")
+    return (name + " is a " + age + " years old " + gender + " born in " + date + " with " + color + " eyes")
 
 
 # Do not edit below this line
-print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
+print(render_person('Bob', '05/22/1983', 'green', '23', 'male'))
