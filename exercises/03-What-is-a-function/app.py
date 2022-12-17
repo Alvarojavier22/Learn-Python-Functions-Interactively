bb7e06edb3501a78dfceb0a583da0b03fa491720
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-12-17 14:56:50 +0000

    Terminado

--- exercises/03-What-is-a-function/app.py
+++ exercises/03-What-is-a-function/app.py
@@ -1,11 +1,5 @@
 def sum(number1,number2):
     return number1 + number2
 
-total = sum(2,3)
-print(total)
-
-def super_duper(number1,number2):
-    return number1 + number2
-
-total = sum(3445324,53454423)
-print(total)
+super_duper = sum(3445324,53454423)
+print(super_duper)
